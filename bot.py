e47967625c80404e3ede6b4c519be06e64bdeffd
Nabil-Alhanif <nabilalhanif03@protonmail.com>
2022-03-31 13:15:53 +0700

    Alert on message deletion

--- bot.py
+++ bot.py
@@ -6,3 +6,12 @@ class CustomBot(commands.Bot):
 
     async def on_message(self, message):
         await self.process_commands(message)
+
+    async def on_message_delete(self, message):
+        channel = message.channel
+
+        # If it's a message from bot, ignore it
+        if message.author.bot:
+            return
+
+        await channel.send(f'The following message from {message.author} has been deleted!\n\n{message.content}')
