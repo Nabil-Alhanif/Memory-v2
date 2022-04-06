6106dced09419bf1b6dc64f3f5efaa9e3fcfaca2
Nabil-Alhanif <nabilalhanif03@protonmail.com>
2022-04-06 08:31:03 +0700

    Add more option for group creation

--- cogs/random/randomizer.py
+++ cogs/random/randomizer.py
@@ -6,8 +6,8 @@ class Randomizer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="rgroup", help="Separate member into groups randomly")
-    async def rgroup(self, ctx, group_size: int, *args) -> None:
+    @commands.command(name="rgs", help="Separate member into groups randomly with n size")
+    async def rgs(self, ctx, group_size: int, *args) -> None:
         selection = []
         for arg in args:
             selection.append(str(arg))
@@ -24,7 +24,39 @@ class Randomizer(commands.Cog):
             groups.append(current_group)
 
         # Generate the result string
-        res = f'Hey {ctx.author.mention}, here are the results of {selection_length} selection(s) divided into groups each with {group_size} size!'
+        res = f'Hey {ctx.author.mention}, here are the results of {selection_length} member(s) divided into groups each with {group_size} member(s)!'
+
+        for i in range(0, len(groups)):
+            res += f'\n\nGroup {i + 1}:'
+            for sel in groups[i]:
+                res += f'\n> {sel}'
+
+        await ctx.send(res)
+        return
+
+    @commands.command(name="rgg", help="Separate member into n group randomly")
+    async def rgg(self, ctx, group_count: int, *args) -> None:
+        selection = []
+        for arg in args:
+            selection.append(str(arg))
+
+        selection_length = len(selection)
+
+        random.shuffle(selection)
+
+        group_size = int(selection_length / group_count)
+        if selection_length % group_count != 0:
+            group_size += 1
+
+        groups = []
+        for i in range(0, selection_length, group_size):
+            current_group = []
+            for j in range(i, min(i + group_size, selection_length)):
+                current_group.append(selection[j])
+            groups.append(current_group)
+
+        # Generate the result string
+        res = f'Hey {ctx.author.mention}, here are the results of {selection_length} member(s) divided into groups each with {group_size} member(s)!'
 
         for i in range(0, len(groups)):
             res += f'\n\nGroup {i + 1}:'
